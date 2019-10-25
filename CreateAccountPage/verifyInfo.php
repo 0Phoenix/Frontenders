45e390064ce81126a0ecdc6e7c926170fd6f4ff9
isaacthomas344 <55213128+isaacthomas344@users.noreply.github.com>
2019-10-25 02:10:40 -0500

    GREATLY reduced the error checking, more readable

---
+++ CreateAccountPage/verifyInfo.php
@@ -0,0 +1,42 @@
+<?php/*
+    //User class
+    class Users {
+      private $firstname;
+      private $lastname;
+      private $age;
+      private $password;
+      private $gender;
+      private $anon;
+
+        function __constuct($afirstname, $alastname, $aage, $apassword, $agender, $aanon){
+          $this->firstname = $afirstname;
+          $this->lastname = $alastname;
+          $this->age = $aage;
+          $this->password = $apassword;
+          $this->gender = $agender;
+          $this->anon = $aanon;
+          echo "New User Created<br>";
+        }
+
+        //Getters
+        function getFirstname(){
+          return $this->firstname;
+        }
+        function getLastname(){
+          return $this->lastname;
+        }
+        function getAge(){
+          return $this->age;
+        }
+        function getPassowrd(){
+          return $this->password;
+        }
+
+        function getGender(){
+          return $this->gender;
+        }
+        function getAnon(){
+          return $this->anon;
+        }
+      }*/
+      ?>
