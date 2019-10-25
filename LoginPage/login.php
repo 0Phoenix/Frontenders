0c9f02c3ac540d37f97bc1a999933a35e8788459
isaacthomas344 <55213128+isaacthomas344@users.noreply.github.com>
2019-10-25 02:47:10 -0500

    Added Login page, This also connects to DB

--- LoginPage/login.php
+++ LoginPage/login.php
@@ -1,10 +1,40 @@
-<!DOCTYPE html>
 <html>
   <body>
     <?php include '../header.html' ?>
     <h3>Login</h3>
 
-    <form action="submitLogin.php" method="post">
+    <?php
+      //starts session
+      session_start();
+
+      //Checks if user is already loggedin
+      if(isset($_SESSION['username']) && isset($_SESSION['loggedin'])){
+        header("Location: users-index.php");
+      }
+
+      //Once user submits
+      if(isset($_POST['login'])){
+        $conn = new mysqli("localhost", "root", "", "users");
+
+        $username = $conn->real_escape_string($_POST['username']);
+        $password = $conn->real_escape_string($_POST['password']);
+
+        $data = $conn->query("SELECT username FROM users WHERE username = '$username' AND password = '$password'");
+
+        //User was found
+        if($data->num_rows > 0) {
+          $_SESSION['username'] = $username;
+          $_SESSION['loggedin'] = 1;
+
+          header("Location: users-index.php");
+          exit();
+        } else {
+          echo "Incorrect Username or Password:";
+        }
+
+      }?>
+
+    <form action="login.php" method="post">
 	     Username:<br>
 	     <input type="text" name="username">
 	     <br>
---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="UTF-8">
+    <meta name="description" content="This is an awesome web">
+
+  </head>
+  <body>
+    <?php include 'header.html' ?>
+
+  </body>
+</html>
