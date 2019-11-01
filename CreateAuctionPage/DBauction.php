b0d44792559f685c08d4b7516e992da1cce12c91
isaacthomas344 <55213128+isaacthomas344@users.noreply.github.com>
2019-11-01 16:57:47 -0500

    create Auction page. Currently not working

---
+++ CreateAuctionPage/DBauction.php
@@ -0,0 +1,43 @@
+<?php
+
+  //After verifying
+  if(isset($_POST["createAuction"])) {
+  //MySQL server info
+  $servername = "localhost";
+  $SQLusername = "root";
+  $SQLpassword = "";
+  $DBname = "frontyardsale";
+
+  //Create connection
+  $conn = new mysqli($servername, $SQLusername, $SQLpassword, $DBname);
+
+  //Check connection
+  if($conn->connect_error){
+    die("Connection failed: " . $conn->connect->error);
+  }
+    echo "Connected Successfully<br>";
+
+  $productTitle = $conn->real_escape_string($_POST['productTitle']);
+  $productDescription = $conn->real_escape_string($_POST['productDescription']);
+  $startprice = $conn->real_escape_string($_POST['startprice']);
+  $bidtime = $conn->real_escape_string($_POST['bidtime']);
+
+
+  if(isset($_POST['anon'])){
+    $anon = $conn->real_escape_string($_POST['anon']);
+  } else {
+    $anon = "No";
+  }
+
+  //Add user to the MySQL database
+  $sql = "INSERT INTO auction (productTitle, productDescription, startprice, bidtime)
+  VALUES ('$productTitle', '$productDescription', '$startprice', '$bidtime')";
+
+  if($conn->query($sql) === TRUE) {
+    echo "Auction created successfully";
+  } else {
+    echo "Error: " . $sql . "<br>" . $conn->error;
+  }
+  $conn->close();
+}
+?>
