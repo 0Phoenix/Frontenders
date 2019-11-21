5a58169729e415602716cfd8294b9df67e7b24aa
isaacthomas344 <55213128+isaacthomas344@users.noreply.github.com>
2019-11-20 21:27:14 -0600

    Deleting all dup files

--- LoginPage/logincheck.php
+++
@@ -1,8 +0,0 @@
-<?php
-  session_start();
-
-  if(!isset($_SESSION['username']) || !isset($_SESSION['loggedin'])){
-    header("Location: login.php");
-    exit();
-  }
-  ?>
--- LoginPage/users-index.php
+++
@@ -1,9 +0,0 @@
-<?php
-  require 'logincheck.php'
-?>
-<html>
-  <body>
-    <?php include '../header.html'; ?>
-      Welcome <?php echo $_SESSION['username'] ?>! Ready to bid?
-  </body>
-<html>
